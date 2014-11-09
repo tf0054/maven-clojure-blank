2165ceaf94c8bb21e3fc990a122e0bee99c10b4b
Takeshi Nakano <tf0054@gmail.com>
2014-11-09 22:03:39 +0100

    Added other ways of calling functions from java

--- src/main/clojure/am/ik/blank/hello.clj
+++ src/main/clojure/am/ik/blank/hello.clj
@@ -1,5 +1,11 @@
 (ns am.ik.blank.hello
-	(:gen-class
-:methods [#^{:static true} [foo [int] void]]))
+   (:gen-class
+	:methods [
+		#^{:static true} [foo [int] void]
+		[echo [String] String]
+	]))
+
+; http://d.hatena.ne.jp/Kazuhira/20121027/1351328382
 
 (defn -foo [i] (println "Hello from Clojure. My input was " i))
+(defn -echo [this s] (str "Echoed from Clojure. The input was " s))
--- src/main/java/am/ik/blank/Hallo.java
+++ src/main/java/am/ik/blank/Hallo.java
@@ -1,8 +1,11 @@
 package am.ik.blank;
 
-//import clojure.lang.RT;
-//import clojure.lang.Symbol;
-//import clojure.lang.Var;
+
+import clojure.lang.RT;
+import clojure.lang.Var;
+import clojure.lang.Symbol;
+import clojure.lang.Compiler;
+import java.io.StringReader;
 
 import am.ik.blank.hello;
 
@@ -14,7 +17,23 @@ public class Hallo {
     public static void main(String[] args) {
         System.out.println(hello("Clojure"));
         hello.foo(1);
-//        IFn plus = Clojure.var("clojure.core", "+");
-//        System.out.println(plus.invoke(1, 2));
+	hello h = new hello();
+        System.out.println(h.echo("Dr.Bones"));
+
+    //
+    // http://stackoverflow.com/a/2182608
+    //
+    // Load the Clojure script -- as a side effect this initializes the runtime.
+    String str = "(ns user) (defn foo [a b]   (str a \" \" b))";
+
+    //RT.loadResourceScript("foo.clj");
+    Compiler.load(new StringReader(str));
+
+    // Get a reference to the foo function.
+    Var foo = RT.var("user", "foo");
+
+    // Call it!
+    Object result = foo.invoke("Hi", "there");
+    System.out.println(result);
     }
 }
