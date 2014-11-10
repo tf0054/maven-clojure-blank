48788596d9639aba8afd3e7ed37bd553f2a7926c
Takeshi Nakano <tf0054@gmail.com>
2014-11-10 16:58:30 +0100

    Added KafkaReading function (alpha)

--- src/main/java/am/ik/blank/StringReaderInputStream.java
+++ src/main/java/am/ik/blank/StringReaderInputStream.java
@@ -4,6 +4,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+
 import java.util.UUID;
 //import java.lang.InterruptedException;
 
@@ -11,11 +22,36 @@ public class StringReaderInputStream extends InputStream
 {
     private String tmpStr;
     private StringReader reader;
+
+    private long count;
+    private final ConsumerConnector consumer;
+    private final String topic;
+
+    private Map<String, List<KafkaStream<byte[], byte[]>>> consumerStreams;
+    private List<KafkaStream<byte[], byte[]>> streams;
     
     public StringReaderInputStream(StringReader reader)
     {
         super();
         this.reader = reader;
+
+        // https://github.com/bkimminich/apache-kafka-book-examples/blob/master/src/test/kafka/consumer/SimpleHLConsumer.java
+        Properties props = new Properties();
+        props.put("zookeeper.connect", "internal-vagrant.genn.ai:2181");
+        props.put("group.id", "samoa-kafka");
+        props.put("zookeeper.session.timeout.ms", "500");
+        props.put("zookeeper.sync.time.ms", "250");
+        props.put("auto.commit.interval.ms", "1000");
+
+        this.topic = "samoa-test";
+
+        consumer = Consumer.createJavaConsumerConnector(new ConsumerConfig(props));
+
+	Map<String, Integer> topicCount = new HashMap<>();
+	topicCount.put(topic, 1);
+
+	consumerStreams = consumer.createMessageStreams(topicCount);
+	List<KafkaStream<byte[], byte[]>> streams = consumerStreams.get(topic);
     }
     
     public StringReaderInputStream(String value)
@@ -26,16 +62,24 @@ public class StringReaderInputStream extends InputStream
     public int read() throws IOException
     {
 	int tmpInt = reader.read();
+
 	if(tmpInt == -1){
-/*
-try {
-	Thread.sleep(1000);
-} catch(InterruptedException e) {
-	System.out.println(e);
-}
-*/
-		reader = new StringReader(UUID.randomUUID()+"\n");
-		tmpInt = reader.read();
+
+            String strLine = "";
+            streams = consumerStreams.get(topic);
+            for (final KafkaStream stream : streams) {
+                ConsumerIterator<byte[], byte[]> it = stream.iterator();
+                if (it.hasNext()) {
+                    strLine = new String(it.next().message());
+		    reader = new StringReader(strLine);
+                    System.out.println("Message from Single Topic: " + strLine + " ("+count+")");
+                    count++;
+                }else{
+                    System.out.println("There is no Message");
+                }
+            }
+
+	    tmpInt = reader.read();
 	}
 	return tmpInt;
     }
