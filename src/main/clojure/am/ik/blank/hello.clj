9c706e47b5bc14086c118cd52214c6e23a032485
Takeshi Nakano <tf0054@gmail.com>
2014-11-21 22:40:13 +0100

    Formatted.

--- src/main/clojure/am/ik/blank/hello.clj
+++ src/main/clojure/am/ik/blank/hello.clj
@@ -7,5 +7,8 @@
 
 ; http://d.hatena.ne.jp/Kazuhira/20121027/1351328382
 
-(defn -foo [i] (println "Hello from Clojure. My input was " i))
-(defn -echo [this s] (str "Echoed from Clojure. The input was " s))
+(defn -foo [s]
+  (println "Hello from Clojure. I am static, My input int was " s ))
+
+(defn -echo [this s] (println "passwd first arg for non-static func: " (type this))
+					  (str "Echoed from Clojure. The input was " s))
--- src/main/java/am/ik/blank/Hallo.java
+++ src/main/java/am/ik/blank/Hallo.java
@@ -10,30 +10,35 @@ import java.io.StringReader;
 import am.ik.blank.hello;
 
 public class Hallo {
-    public static String hello(String name) {
-        return "Hello " + name + "!";
+
+    public static String static_java(String name) {
+        return "Hello " + name + " from java!";
     }
-    
+
     public static void main(String[] args) {
-        System.out.println(hello("Clojure"));
-        hello.foo(1);
-	hello h = new hello();
+        System.out.println(static_java("Clojure"));
+
+        // clojure static func call
+        hello.foo(12);
+
+        // clojure non-static func call
+        hello h = new hello();
         System.out.println(h.echo("Dr.Bones"));
 
-    //
-    // http://stackoverflow.com/a/2182608
-    //
-    // Load the Clojure script -- as a side effect this initializes the runtime.
-    String str = "(ns user) (defn foo [a b]   (str a \" \" b))";
+        //
+        // http://stackoverflow.com/a/2182608
+        //
+        // Load the Clojure script -- as a side effect this initializes the runtime.
+        String str = "(ns user) (defn foo [a b]   (str a \" \" b))";
 
-    //RT.loadResourceScript("foo.clj");
-    Compiler.load(new StringReader(str));
+        //RT.loadResourceScript("foo.clj");
+        Compiler.load(new StringReader(str));
 
-    // Get a reference to the foo function.
-    Var foo = RT.var("user", "foo");
+        // Get a reference to the foo function.
+        Var foo = RT.var("user", "foo");
 
-    // Call it!
-    Object result = foo.invoke("Hi", "there");
-    System.out.println(result);
+        // Call it!
+        Object result = foo.invoke("Hi", "there");
+        System.out.println(result);
     }
 }
